4a18b8604ade57f01fa3fda0fb5d39568d756671
George Allison <george@engr2-22-166-dhcp.int.colorado.edu>
2018-09-13 11:04:21 -0600

    first attempt at working on odometer logic

--- lab_2/lab_2/lab_2.ino
+++ lab_2/lab_2/lab_2.ino
@@ -1,38 +1,71 @@
 #include <Sparki.h> 
-unsigned long time;
+
+unsigned long timeStart; //global varts for timing
+unsigned long timeEnd;
+unsigned long totalTime = 0;
+
+long speed = 2.73 //cm per second
+
+int threshold = 500; //global vars for IR sensor
+int lineLeft;  
+int lineCenter; 
+int lineRight;
+
+double xPos = 0; //global vars for odometry
+double yPos = 0;
+double theta = 0;
+double leftWheel;
+double rightWheel;
+int lastDir;
+
 void setup() 
 {
   Serial.begin(9600); //set the data rate to 9600 bits per second for serial data transmission
+  lastDir = 0;
 }
  
 void loop() {
-  Serial.print("Time: ");
-  time = millis(); //milliseconds elapsed since program start
-  Serial.println(time);
-  
-  int threshold = 500;
-  int lineLeft   = sparki.lineLeft();  
-  int lineCenter = sparki.lineCenter(); 
-  int lineRight  = sparki.lineRight(); 
+  lineLeft   = sparki.lineLeft();  
+  lineCenter = sparki.lineCenter(); 
+  lineRight  = sparki.lineRight();
  
-  if ( lineLeft < threshold ) 
-  {  
+  if (lineLeft < threshold){ 
     sparki.moveLeft();
-  }
- 
-  if ( lineRight < threshold ) 
-  {  
+    //left movement is 2.73 cm/s speed for 0.1 sec; 0.273 cm of movement for each wheel
+    leftWheel -= 0.273;
+    rightWheel += 0.273;
+    lastDir = 1;
+  }else if (lineRight < threshold) {  
     sparki.moveRight();
+    //left movement is 2.73 cm/s speed for 0.1 sec; 0.273 cm of movement for each wheel
+    leftWheel += 0.273;
+    rightWheel -= 0.273;
+    lastDir = 2;
+  }else if ((lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold)){
+    sparki.moveForward();
+    leftWheel += 0.273;
+    rightWheel += 0.273;
+    lastDir = 3;
   }
- 
-  
-  if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
-  {
-    sparki.moveForward(); 
-  }  
+
+  if(lastDir == 1){
+      //if last direction was forward
+      xPos += 0.273;
+      //y pos does not change
+      //theta does not change
+    } else if (lastDir == 2) {
+      /*
+        change theta and add distance of rotated movement
+      */
+    } else if (lastDir == 3) {
+      /*
+        change theta and add distance of rotated movement
+      */
+    } else {
+      sparki.println("logic error 1");
+  }
  
   sparki.clearLCD();
- 
   sparki.print("Line Left: ");
   sparki.println(lineLeft);
  
